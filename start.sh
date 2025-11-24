f040dfbf1d3d18da9815e761c479959a29e4556d
Enildo-Martins <enildooliveira1@gmail.com>
2025-11-23 22:33:40 -0300

    fix: Inicia Celery junto com Gunicorn no start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-# Sai do script se qualquer comando der erro
+# Sai do script se der erro em qualquer comando
 set -e
 
 echo "Rodando Migrations..."
 python manage.py migrate
 
-echo "Iniciando Gunicorn..."
+echo "Iniciando Celery Worker em Background..."
+# O '&' no final faz ele rodar em segundo plano
+# Usamos --concurrency=1 para gastar pouca memória do servidor grátis
+celery -A core worker --loglevel=info --concurrency=1 &
+
+echo "Iniciando Gunicorn (Servidor Web)..."
 # O Render injeta a variável PORT automaticamente
 gunicorn core.wsgi:application --bind 0.0.0.0:$PORT
